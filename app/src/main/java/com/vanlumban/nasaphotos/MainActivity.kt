0d23166710e8a227e4ff50619ce4c13afcf37701
Van Joseph Lumban <lumban.vjm@gmail.com>
2025-08-28 17:17:28 +0800

    Remove unused imports

--- app/src/main/java/com/vanlumban/nasaphotos/MainActivity.kt
+++ app/src/main/java/com/vanlumban/nasaphotos/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
--- app/src/main/java/com/vanlumban/nasaphotos/data/models/NasaPhoto.kt
+++ app/src/main/java/com/vanlumban/nasaphotos/data/models/NasaPhoto.kt
@@ -1,6 +1,5 @@
 package com.vanlumban.nasaphotos.data.models
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
